19ece04ca7120fe05c5ef43c7405f296d21ab9d5
haithem <haithemboubakri42@gmail.com>
2023-04-05 02:00:37 +0200

    jwt signup

---
+++ server/controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// login a user
+const loginUser = async (req, res) => {
+  res.json({mssg: 'login user'})
+}
+
+// signup a user
+const signupUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.signup(email, password)
+
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+module.exports = { signupUser, loginUser }
---
+++ server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const expresss = require("express")
+const mongoose = require("mongoose")
+const authRoutes = require("./routes/authRoutes")
+
+const app = expresss()
+
+
+app.use(expresss.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/api/auth', authRoutes)
+
+mongoose.connect(process.env.DB_URI)
+    .then(()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log('connected to DB, listening on port: ', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
